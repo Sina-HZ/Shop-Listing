3bd95abccc6ac4d90998e84ed55671c4125a5227
Sina <s.hoseinzadeh@ejobsco.com>
2024-12-06 11:07:27 +0330

    add basename for browser router

--- src/routes.tsx
+++ src/routes.tsx
@@ -5,7 +5,7 @@ const AsyncShoppingList = React.lazy(() => import("@/views/ShoppingList"));
 
 const AppRoutes = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/Shop-Listing">
       <Routes>
         <Route path="/" element={<AsyncShoppingList />} />
         <Route path="/*" element={<p>page not found</p>} />
